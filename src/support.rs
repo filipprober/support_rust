71e5f3724c7a9cf0e7a47329f92d9b7c86d705c3
filip-codes <filip@bitecorn.com>
2025-09-12 23:09:20 +0200

    feat: added String.after() method

--- src/support.rs
+++ src/support.rs
@@ -1,4 +1,19 @@
+use std::fmt::Display;
+
 pub trait Strings {
+    ///
+    /// Return the remainder of a string after the first occurrence of a given value.
+    ///
+    /// # Usage
+    ///
+    /// ```
+    /// use support::Strings;
+    ///
+    /// "Rust Support".after("Rust ");
+    /// // "Support"
+    /// ```
+    fn after<T: Display>(&self, search: T) -> String;
+
     ///
     /// Reverses the given string.
     ///
@@ -8,18 +23,33 @@ pub trait Strings {
     /// use support::Strings;
     ///
     /// "Rust Support".reverse();
-    /// // Output: "troppuS tsuR"
+    /// // "troppuS tsuR"
     /// ```
     fn reverse(&self) -> String;
 }
 
 impl Strings for str {
+    fn after<T: Display>(&self, search: T) -> String {
+        let search = search.to_string();
+        match self.find(&search) {
+            Some(index) => {
+                let start = index + search.len();
+                self[start..].to_string()
+            }
+            None => self.to_string(),
+        }
+    }
+
     fn reverse(&self) -> String {
         self.chars().rev().collect()
     }
 }
 
 impl Strings for String {
+    fn after<T: Display>(&self, search: T) -> String {
+        self.as_str().after(search)
+    }
+
     fn reverse(&self) -> String {
         self.as_str().reverse()
     }
--- tests/string_tests.rs
+++ tests/string_tests.rs
@@ -5,6 +5,18 @@ mod string_tests {
     use super::*;
 
     #[test]
+    fn after() {
+        assert_eq!("nah", "hannah".after("han"));
+        assert_eq!("nah", "hannah".after("n"));
+        assert_eq!("nah", "ééé hannah".after("han"));
+        assert_eq!("hannah", "hannah".after("xxxx"));
+        assert_eq!("hannah", "hannah".after(""));
+        assert_eq!("nah", "han0nah".after("0"));
+        assert_eq!("nah", "han0nah".after(0));
+        assert_eq!("nah", "han2nah".after(2));
+    }
+
+    #[test]
     fn reverse() {
         assert_eq!("FooBar", "raBooF".reverse());
         assert_eq!("Teniszütő", "őtüzsineT".reverse());
